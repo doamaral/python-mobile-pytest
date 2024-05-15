c59efed876f38497ef475cc431a5f316ceba925f
doamaral <lopesdoamaral@gmail.com>
2024-05-14 21:17:28 -0300

    Add logs to Identify which props are being used

--- core/FakeAppiumDriver.py
+++ core/FakeAppiumDriver.py
@@ -2,6 +2,11 @@ class FakeAppiumDriver:
     def __init__(self, desired_caps):
         print('Initializing FakeAppiumDriver')
         self._desired_caps = desired_caps
+        self._platformName = desired_caps.get('platformName')
+        self._deviceName = desired_caps.get('deviceName')
+        
+        print(f'Platform name: {self._platformName}')
+        print(f'Device name: {self._deviceName}')
 
     def find_element_by_id(self, id):
         print(f'Finding element by id: {id}')
