3a2849aa0ad369e9fd1197ae64ef3257085abc8a
doamaral <lopesdoamaral@gmail.com>
2024-05-14 21:16:31 -0300

    Improve test to check platform against env var

--- test_driverfactory.py
+++ test_driverfactory.py
@@ -1,7 +1,12 @@
 import pytest
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def test_driver_factory(setup):
-    print('Test')
     driver = setup
+    print(f'This test is running over: {driver.__getattribute__("_platformName")}')
     assert driver is not None
+    assert driver.__getattribute__('_platformName').lower() == os.getenv('PLATFORM').lower()
     
