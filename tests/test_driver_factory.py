511c8614592b121814dfd7e3193565f8d70a8f1e
lucas amaral <os-lucas.amaral@rakuten.com>
2024-05-23 09:49:56 -0300

    Use the page object to exemplify the Platform awareness

--- tests/test_driver_factory.py
+++ tests/test_driver_factory.py
@@ -1,5 +1,6 @@
 from os import getenv
 from dotenv import load_dotenv
+from page_objects.home_page import HomePage
 
 from utils.mock_appium_driver import MockAppiumDriver
 load_dotenv()
@@ -10,3 +11,5 @@ def test_driver_factory(setup: MockAppiumDriver):
     print(f'This test is running over: {driver.get_platform_name()}')
     assert driver is not None
     assert driver.get_platform_name().lower() == getenv('PLATFORM').lower()
+    home_page = HomePage(driver)
+    home_page.click_login()
