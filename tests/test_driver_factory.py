354a08547403a78cf9943cfd1d7766460925607d
lucas amaral <os-lucas.amaral@rakuten.com>
2024-05-23 12:13:04 -0300

    add test

--- tests/test_driver_factory.py
+++ tests/test_driver_factory.py
@@ -6,11 +6,16 @@ from utils.mock_appium_driver import MockAppiumDriver
 load_dotenv(override=False)
 
 
-def test_driver_factory(setup: MockAppiumDriver):
-    driver = setup
+def test_driver_factory(setup_driver: MockAppiumDriver):
+
+    driver = setup_driver
     print(f'[test_driver_factory] This test is running over: {
           driver.get_platform_name()}')
     assert driver is not None
     assert driver.get_platform_name().lower() == getenv('PLATFORM').lower()
+
+
+def test_page_objects_environment():
     home_page = HomePage()
     home_page.click_login()
+    assert getenv('PLATFORM').lower() == home_page.get_platform()
