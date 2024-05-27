23d9ef61676d47eb7e95f72fe4e2297b3fcb5d06
lucas amaral <os-lucas.amaral@rakuten.com>
2024-05-27 01:01:57 -0300

    Make Android read from .env file

--- core/android_driver.py
+++ core/android_driver.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
@@ -6,10 +8,14 @@ class AndroidDriver:
     __android_desired_caps = {}
 
     def __init__(self):
+        load_dotenv()
         self.__android_desired_caps = UiAutomator2Options()
-        self.__android_desired_caps.platform_version = '14.0'
-        self.__android_desired_caps.device_name = 'Pixel_3a_API_34'
-        self.__appium_server_url = 'http://localhost:4723'
+        self.__android_desired_caps.platform_version = os.getenv(
+            'ANDROID_PLATFORM_VERSION', '14.0')
+        self.__android_desired_caps.device_name = os.getenv(
+            'ANDROID_DEVICE_NAME', 'Pixel_3a_API_34')
+        self.__appium_server_url = os.getenv(
+            'APPIUM_SERVER_URL', 'http://localhost:4723')
 
     def create_driver(self):
         print('[AndroidDriver] Initializing')
