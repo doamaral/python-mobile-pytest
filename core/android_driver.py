6777c7f5aff2c33d1c309db8c2094b105e749b3a
lucas amaral <os-lucas.amaral@rakuten.com>
2024-05-27 11:29:16 -0300

    Initiate desired caps and set app path

--- core/android_driver.py
+++ core/android_driver.py
@@ -5,15 +5,20 @@ from appium.options.android import UiAutomator2Options
 
 
 class AndroidDriver:
-    __android_desired_caps = {}
+    __android_desired_caps = None
 
     def __init__(self):
         load_dotenv()
+
         self.__android_desired_caps = UiAutomator2Options()
+
         self.__android_desired_caps.platform_version = os.getenv(
             'ANDROID_PLATFORM_VERSION', '14.0')
         self.__android_desired_caps.device_name = os.getenv(
             'ANDROID_DEVICE_NAME', 'Pixel_3a_API_34')
+
+        self.__android_desired_caps.app = os.getcwd() + \
+            "/app/android.wdio.native.app.v1.0.8.apk"
         self.__appium_server_url = os.getenv(
             'APPIUM_SERVER_URL', 'http://localhost:4723')
 
