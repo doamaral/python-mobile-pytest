38afb3bfa26ff0a89ac39809a64a3844d6e66659
lucas amaral <os-lucas.amaral@rakuten.com>
2024-05-27 00:37:55 -0300

    Set MockDriver to be closer to the real driver object

--- utils/mock_appium_driver.py
+++ utils/mock_appium_driver.py
@@ -1,14 +1,6 @@
 class MockAppiumDriver:
     def __init__(self, desired_caps):
-        self._desired_caps = desired_caps
-        self._platform_name = desired_caps.get('platformName')
-        self._device_name = desired_caps.get('deviceName')
-        print(f'[MockAppiumDriver] Setting desired capabilities for: {
-              self._platform_name}')
-        print(f'[MockAppiumDriver] Device name: {self._device_name}')
-
-    def get_platform_name(self):
-        return self._platform_name
+        self.capabilities = desired_caps
 
     def find_element_by_id(self, element_id):
         print(f'[MockAppiumDriver] Finding element by id: {element_id}')
